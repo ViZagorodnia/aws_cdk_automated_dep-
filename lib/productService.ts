9a40146df1f8b5c8dea573e721bf2394ad37f1e3
ViZagorodnia <viktoriazagorodnia@gmail.com>
2024-11-05 15:02:21 +0100

    fixed productService

--- lib/productService.ts
+++ lib/productService.ts
@@ -141,23 +141,6 @@ export class ProductServiceStack extends cdk.Stack {
         
         productsTable.grantWriteData(createProductLambdaFn);
         stockTable.grantWriteData(createProductLambdaFn);
-
-
-
-        // // Define IAM role for Lambda functions
-        // const lambdaRole = new iam.Role(this, 'LambdaExecutionRole', {
-        //     assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-        //     managedPolicies: [
-        //         iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
-        //         iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'), // Consider using more restrictive policies in production
-        //     ],
-        // });
-        
-        // // Add policy for access to S3 resources (as needed)
-        // lambdaRole.addToPolicy(new iam.PolicyStatement({
-        //     actions: ['s3:GetObject'],
-        //     resources: ['arn:aws:s3:::deploywebappstack-deploymentfrontendbucket67ceb713-dmeuplpxznej/*'],
-        // }));
     
     }
 }
